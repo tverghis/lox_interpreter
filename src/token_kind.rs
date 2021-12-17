0bdf3409178d91d711b7926e06e98eb9692cab96
Tarun Verghis <tarun.verghis@gmail.com>
2021-12-17 00:55:51 -0800

    Add TokenKind
    TokenKind is an enum representing the types of tokens that the scanner
    can encounter. Most TokenKind variants do not have any associated data,
    but the variants representing literals do (they store the literal
    value).

---
+++ src/token_kind.rs
@@ -0,0 +1,51 @@
+#[derive(Debug)]
+pub enum TokenKind<'a> {
+    // Single-character tokens
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One- or two-character tokens
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals
+    Identifier(&'a str),
+    String(&'a str),
+    Number(f64),
+
+    // Keywords
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    // EOF
+    Eof,
+}
