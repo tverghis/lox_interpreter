8dec4c85597a13fdb7db9940335ab86da7059586
Tarun Verghis <tarun.verghis@gmail.com>
2021-12-17 00:57:30 -0800

    Flesh out Token struct
    The Token struct will be used to keep track of the identified lexemes,
    as well as some meta-information, like the TokenKind and the line on
    which it was encountered.
    
    This patch adds those fields to the Token struct, and allows
    construction via a `new` function.

--- src/token.rs
+++ src/token.rs
@@ -1,2 +1,14 @@
+use crate::token_kind::TokenKind;
+
 #[derive(Debug)]
-pub struct Token;
+pub struct Token<'a> {
+    kind: TokenKind<'a>,
+    lexeme: &'a str,
+    line: u32,
+}
+
+impl<'a> Token<'a> {
+    pub fn new(kind: TokenKind<'a>, lexeme: &'a str, line: u32) -> Self {
+        Token { kind, lexeme, line }
+    }
+}
